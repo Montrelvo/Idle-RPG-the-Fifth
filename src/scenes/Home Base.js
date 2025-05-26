c1e8ebfe2414e052cb889f35c080040be3b2e6a3
Montrelvo <36254931+Montrelvo@users.noreply.github.com>
2025-05-25 21:49:32 -0400

    Empty scene added for Home Base

---
+++ src/scenes/Home Base.js
@@ -0,0 +1,19 @@
+// "Every great game begins with a single scene. Let's make this one unforgettable!"
+export class HomeBase extends Phaser.Scene {
+    constructor() {
+        super('HomeBase');
+    }
+
+    init() {
+        // Initialize scene
+    }
+
+    preload() {
+        // Load assets
+    }
+
+    create() {
+        // Create game objects
+    }
+
+}
